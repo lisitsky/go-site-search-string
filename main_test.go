748aa1478c6a2e409d8a2ae1a108ed9ce235965f
Eugene Lisitsky <lisitsky@gmail.com>
2017-07-22 00:00:28 +0300

    Add test for main()

--- main_test.go
+++ main_test.go
@@ -413,3 +413,24 @@ func TestChecksSitesHandler_InvalidJSON(t *testing.T) {
 		"Should return status:bad request")
 
 }
+
+func TestMainExecution(t *testing.T) {
+	go main()
+	resp, err := http.Get("http://127.0.0.1:8080/checkHealth") //TODO move port to config. Where to get addr?
+	if err != nil {
+		t.Fatalf("Cannot make get: %v\n", err)
+	}
+	bodySb, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Error reading body: %v\n", err)
+	}
+	body := string(bodySb)
+	fmt.Printf("Body: %v\n", body)
+	var decodedResponse interface{}
+	err = json.Unmarshal(bodySb, &decodedResponse)
+	if err != nil {
+		t.Fatalf("Cannot decode response <%p> from server. Err: %v", bodySb, err)
+	}
+	assert.Equal(t, map[string]interface{}{"status": "ok"}, decodedResponse,
+		"Should return status:ok")
+}
